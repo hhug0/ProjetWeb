d49a7a2edb6c9dc71a94df481792f85e89cd2d93
hhug0 <113453230+hhug0@users.noreply.github.com>
2023-07-07 16:32:56 +0200

    soutenance

--- projetquizz/users/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    try 
-    {
-        $bdd = new PDO("mysql:host=localhost;dbname=quizzeo;charset=utf8", "root", "");
-    }
-    catch(PDOException $e)
-    {
-        die('Erreur : '.$e->getMessage());
-    }
